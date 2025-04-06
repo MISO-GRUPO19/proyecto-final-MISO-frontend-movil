60bcd76ddeacba16adafe390cfd3bfc6a959844d
Carlos Tiguaque <c.tiguaque@uniandes.edu.co>
2025-04-05 20:13:57 -0500

    Se adicionan pruebas unitarias

--- app-movil/src/app/layout/home-page/home-page.component.spec.ts
+++ app-movil/src/app/layout/home-page/home-page.component.spec.ts
@@ -20,4 +20,21 @@ describe('HomePageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set roleId if user data exists in sessionStorage', () => {
+    const mockUser = { role: 2 }; // Simula un rol de usuario
+    sessionStorage.setItem('user', JSON.stringify(mockUser));
+
+    component.ngOnInit();
+
+    expect(component.roleId).toBe(mockUser.role);
+  });
+
+  it('should not set roleId if no user data exists in sessionStorage', () => {
+    sessionStorage.removeItem('user');
+    
+    component.ngOnInit();
+
+    expect(component.roleId).toBeUndefined();
+  });
 });
