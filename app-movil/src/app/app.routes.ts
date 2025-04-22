59e6b7781843ee367e992ac8bdaa79782d20bc18
LuciaColorado <m.colorado@uniandes.edu.co>
2025-04-22 14:28:50 -0500

    Add view Inventory

--- app-movil/src/app/app.routes.ts
+++ app-movil/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomePageComponent } from './layout/home-page/home-page.component';
 import { BaseAuthComponent } from './layout/auth/base-auth/base-auth.component';
 import { ProfileFormComponent } from './modules/profile/profile-form/profile-form.component';
 import { LoginInComponent } from './layout/auth/login-in/login-in.component';
+import { InventoryListComponent } from './modules/inventory/inventory-list/inventory-list.component';
 
 export const routes: Routes = [
     {
@@ -13,7 +14,7 @@ export const routes: Routes = [
             // { path: 'reports', component: ReportsComponent },
             // { path: 'deliveries', loadChildren: () => import('./modules/deliveries/deliveries.routes').then((r) => r.deliveriesRoutes), data: { title: 'Entregas' } },
             // { path: 'profile', component: ProfileComponent },
-            // { path: 'inventory', component: InventoryListComponent, data: { title: 'Inventario' } },
+            { path: 'inventory', component: InventoryListComponent, data: { title: 'Inventario' } },
             // { path: 'visits', component: VisitsComponent },
             // { path: 'shopping-cart', component: ShoppingCartComponent },
             // { path: 'clients', component: ClientsListComponent, data: { title: 'Clientes' } }
---
+++ app-movil/src/app/modules/inventory/services/products.service.ts
@@ -0,0 +1,25 @@
+
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { inject, Injectable } from '@angular/core';
+import { ProductsList } from '../models/inventory.model';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ProductsManager {
+    private apiUrl = environment.apiUrl; // API
+    private http = inject(HttpClient);
+    private headers = {
+        Authorization: `Bearer ${sessionStorage.getItem('access_token')}`
+    };
+
+    constructor() { }
+
+    getProductsInventory(): Observable<ProductsList[]> {
+        return this.http.get<ProductsList[]>(this.apiUrl + '/products', { headers: this.headers })
+    }
+
+
+}
