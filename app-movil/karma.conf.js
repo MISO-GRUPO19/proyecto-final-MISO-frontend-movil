038a2cb8eefbe0fcda9530a93041de6c2cda54cf
LuciaColorado <m.colorado@uniandes.edu.co>
2025-04-22 20:44:25 -0500

    Añadir Tests

---
+++ app-movil/karma.conf.js
@@ -0,0 +1,39 @@
+// karma.conf.js
+module.exports = function (config) {
+    config.set({
+      basePath: '',
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-jasmine-html-reporter'),
+        require('karma-coverage'),
+        require('@angular-devkit/build-angular/plugins/karma')
+      ],
+      client: {
+        jasmine: {
+          // Puedes desactivar la ejecución automática si quieres paso a paso
+          // clearContext: false
+        }
+      },
+      coverageReporter: {
+        dir: require('path').join(__dirname, './coverage'),
+        subdir: '.',
+        reporters: [{ type: 'html' }, { type: 'text-summary' }]
+      },
+      reporters: ['progress', 'kjhtml'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ['ChromeHeadless'],
+      singleRun: false,
+      restartOnFileChange: true,
+  
+      // 🛠️ Clave para tu error:
+      browserDisconnectTimeout: 10000,
+    browserNoActivityTimeout: 120000, 
+    captureTimeout: 120000,
+    });
+  };
+  
